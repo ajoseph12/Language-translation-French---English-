8402d921b429be03568805dc73751b52eab5587c
ajoseph12 <allwyn_don32@yahoo.com>
2018-05-10 15:55:23 +0200

    [Creating] - dataset creation

--- data_utils.py
+++ data_utils.py
@@ -3,12 +3,15 @@
 # |------------------------------------------------|
 # |Dataset: OpenSubtitles                          |
 # |Url: http://opus.lingfil.uu.se/OpenSubtitles.php| 
-# |Creator: Allwyn Joseph 
-# |Inspired by:                          |
+# |Creator: Allwyn Joseph 						   |
+# |Code Inspired by: Nemanja Tomic                 |
 # |------------------------------------------------|
 
 import cPickle as pickle
 from collections import Counter 
+import re
+
+_WordInput = re.compile(',." ;:)(][?!')
 
 
 def read_sentence(file_path):
@@ -28,4 +31,70 @@ def read_sentence(file_path):
 
 		for phrase in reader:
 			phrases.append(phrase)
+
+def create_datasets(en_phrase, fr_phrase):
+	
+	"""
+	This function would extract the words from each of the phrases
+	and store in a sorted manner within a dictionary. The function 
+	further goes on to create word2indx and indx2word dictionaries
+	along with input and target lists.
+
+	Argument:
+	en_phrases -- list of english phrases 
+	fr_phrases -- list of french phrases
+
+	Returns:
+	word2indx --
+	indx2word -- 
+	X -- 
+	Y -- 
+
+	"""
+	sp = ',." ;:)(][?!'
+	
+	# creating a dictionary of unique words with their frequency as the key
+	en_vocab_dict = Counter(word.strip(sp) for phrase in en_phrases for word in phrase.split())
+	fr_vocab_dict = Counter(word.strip(sp) for phrase in fr_phrases for word in phrase.split())
+
+	# creating a list of words in descending order of their frequency 
+	en_vocab = list(map(lambda x: x[0], sorted(en_vocab_dict.items(), key = lambda x: -x[1])))
+	fr_vocab = list(map(lambda x: x[1], sorted(fr_vocab_dict.items(), key = lambda x: -x[1])))
+	
+
+
+	unique()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
